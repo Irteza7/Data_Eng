d6ab42e57abd26860ebce826cc846cb61eac31be
Irteza7 <m.irtezakhan@hotmail.com>
2023-05-04 20:42:56 +0200

    pyql

---
+++ pysql2.py
@@ -0,0 +1,47 @@
+import ibm_db
+import ibm_db_dbi
+import ibm_db_sa
+import sqlalchemy
+import pandas as pd
+
+
+dsn_hostname = "764264db-9824-4b7c-82df-40d1b13897c2.bs2io90l08kqb1od8lcg.databases.appdomain.cloud" # e.g.: "54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+dsn_uid = "ynr01638"        # e.g. "abc12345"
+dsn_pwd = "IXOaPrYHTMmngIQ2"      # e.g. "7dBZ3wWt9XN6$o0J"
+
+dsn_driver = "{IBM DB2 ODBC DRIVER}"
+dsn_database = "BLUDB"            # e.g. "BLUDB"
+dsn_port = "32536"                # e.g. "32733" 
+dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+dsn_security = "SSL"              #i.e. "SSL"
+
+
+#Create the dsn connection string
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd,dsn_security)
+
+#print the connection string to check correct values are specified
+print(dsn)
+
+
+
+#Create database connection
+
+try:
+    engine = sqlalchemy.create_engine(f"ibm_db_sa://{dsn_uid}:{dsn_pwd}@{dsn_hostname}:{dsn_port}/{dsn_database}")
+    print("Connected to database:", dsn_database, "as user:", dsn_uid, "on host:", dsn_hostname)
+except Exception as e:
+    print("Unable to connect:", str(e))
+
+
+
+
+chicago_socioeconomic_data = pd.read_csv('https://data.cityofchicago.org/resource/jcxq-k9xf.csv')
+chicago_socioeconomic_data.to_sql('chicago_socioeconomic_data', engine, schema=YNR01638, if_exists='replace', index=False)
--- pysqlite3.py
+++ pysqlite3.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 conn = sqlite3.connect('INSTRUCTOR.db')
 
@@ -13,3 +14,42 @@ cursor_obj.execute(table)
 cursor_obj.execute('''insert into INSTRUCTOR values (1, 'Rav', 'Ahuja', 'TORONTO', 'CA')''')
 cursor_obj.execute('''insert into INSTRUCTOR values (2, 'Raul', 'Chong', 'Markham', 'CA'), (3, 'Hima', 'Vasudevan', 'Chicago', 'US')''')
 
+statement = '''SELECT * FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+
+print("All the data")
+output_all = cursor_obj.fetchall()
+for row_all in output_all:
+  print(row_all)
+
+
+## Fetch few rows from the table
+statement = '''SELECT * FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+  
+print("All the data")
+# If you want to fetch few rows from the table we use fetchmany(numberofrows) and mention the number how many rows you want to fetch
+output_many = cursor_obj.fetchmany(2) 
+for row_many in output_many:
+  print(row_many)
+
+# Fetch only FNAME from the table
+statement = '''SELECT FNAME FROM INSTRUCTOR'''
+cursor_obj.execute(statement)
+  
+print("All the data")
+output_column = cursor_obj.fetchall()
+for fetch in output_column:
+  print(fetch)
+
+df = pd.read_sql_query("select * from instructor;", conn)
+
+#print the dataframe
+df
+
+conn.close()
+
+
+chicago_socioeconomic_data = pd.read_csv('https://data.cityofchicago.org/resource/jcxq-k9xf.csv')
+chicago_socioeconomic_data.head(5)
+chicago_socioeconomic_data.columns
