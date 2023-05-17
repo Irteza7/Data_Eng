9b54480ac921826be21565a11a07dafa0c5bdf88
Irteza7 <m.irtezakhan@hotmail.com>
2023-05-17 11:42:45 +0200

    dag

--- first_dag.py
+++ first_dag.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 # Operators; we need this to write tasks!
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.bash import BashOperator
 # This makes scheduling easy
 from airflow.utils.dates import days_ago
 
