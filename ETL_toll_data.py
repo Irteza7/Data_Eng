883ad6445061ed9b65761c28bc5d6a64e1a18525
Irteza7 <m.irtezakhan@hotmail.com>
2023-05-19 23:50:34 +0200

    kafka

--- ETL_toll_data.py
+++ ETL_toll_data.py
@@ -29,37 +29,46 @@ dag = DAG(
 
 unzip_data = BashOperator(
     task_id = 'unzip_data',
-    bash_command='tar -xvzf /home/irteza/airflow/dags/finalassignment/tolldata.tgz -C /home/irteza/airflow/dags/finalassignment',
+    bash_command='tar -xvzf /home/irteza/airflow/dags/finalassignment/tolldata.tgz\
+          -C /home/irteza/airflow/dags/finalassignment',
     dag=dag,
 )
 
 extract_data_from_csv = BashOperator(
     task_id = 'extract_data_from_csv',
-    bash_command='cut -d"," -f1-4 /home/irteza/airflow/dags/finalassignment/vehicle-data.csv > /home/irteza/airflow/dags/finalassignment/csv_data.csv',
+    bash_command='cut -d"," -f1-4 /home/irteza/airflow/dags/finalassignment/vehicle-data.csv >\
+          /home/irteza/airflow/dags/finalassignment/csv_data.csv',
     dag=dag,
 )
 
 extract_data_from_tsv = BashOperator(
     task_id = 'extract_data_from_tsv',
-    bash_command='cut -f5-7 /home/irteza/airflow/dags/finalassignment/tollplaza-data.tsv | tr "\t" "," > /home/irteza/airflow/dags/finalassignment/tsv_data.csv',
+    bash_command='cut -f5-7 /home/irteza/airflow/dags/finalassignment/tollplaza-data.tsv | tr "\t" "," >\
+          /home/irteza/airflow/dags/finalassignment/tsv_data.csv',
     dag=dag,
 )
 
 extract_data_from_fixed_width = BashOperator(
     task_id = 'extract_data_from_fixed_width',
-    bash_command='awk \'{print $(NF-1), $NF}\' /home/irteza/airflow/dags/finalassignment/payment-data.txt > /home/irteza/airflow/dags/finalassignment/fixed_width_data.csv',
+    bash_command='awk \'{print $(NF-1), $NF}\' /home/irteza/airflow/dags/finalassignment/payment-data.txt >\
+          /home/irteza/airflow/dags/finalassignment/fixed_width_data.csv',
     dag=dag,
 )
 
 consolidate_data = BashOperator(
     task_id = 'consolidate_data',
-    bash_command='paste /home/irteza/airflow/dags/finalassignment/csv_data.csv /home/irteza/airflow/dags/finalassignment/tsv_data.csv /home/irteza/airflow/dags/finalassignment/fixed_width_data.csv > /home/irteza/airflow/dags/finalassignment/extracted_data.csv',
+    bash_command='paste /home/irteza/airflow/dags/finalassignment/csv_data.csv\
+          /home/irteza/airflow/dags/finalassignment/tsv_data.csv \
+            /home/irteza/airflow/dags/finalassignment/fixed_width_data.csv >\
+                  /home/irteza/airflow/dags/finalassignment/extracted_data.csv',
     dag=dag,
 )
 
 transform_data = BashOperator(
     task_id = 'transform_data',
-    bash_command='awk -F"," -v OFS="," \'{ $4 = toupper($4); print }\' /home/irteza/airflow/dags/finalassignment/extracted_data.csv > /home/irteza/airflow/dags/finalassignment/transformed_data.csv',
+    bash_command='awk -F"," -v OFS="," \'{ $4 = toupper($4); print }\'\
+          /home/irteza/airflow/dags/finalassignment/extracted_data.csv >\
+              /home/irteza/airflow/dags/finalassignment/transformed_data.csv',
     dag=dag,
 )
 
