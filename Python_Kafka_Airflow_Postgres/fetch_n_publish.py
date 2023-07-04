c3379c0af25d1dba12a44d5144e9e5a890c0f1dd
Irteza7 <m.irtezakhan@hotmail.com>
2023-07-04 14:44:04 +0200

    api key removed

--- Python_Kafka_Airflow_Postgres/fetch_n_publish.py
+++ Python_Kafka_Airflow_Postgres/fetch_n_publish.py
@@ -5,7 +5,7 @@ import json
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-api_key = 'f9361d4590585fa8de005eca48935618'
+api_key = '---'
 city_id = '2934246'
 Params = {'appid': api_key , 'id':city_id}
 response = requests.get('http://api.openweathermap.org/data/2.5/weather',params=Params)
