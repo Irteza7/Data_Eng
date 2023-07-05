cf688f3ce8e76494eb51eac4a26a275015389beb
Irteza7 <m.irtezakhan@hotmail.com>
2023-07-05 20:58:50 +0200

    config.ini added to git ignore

--- Python_Kafka_Airflow_Postgres/fetch_n_publish.py
+++ Python_Kafka_Airflow_Postgres/fetch_n_publish.py
@@ -1,11 +1,14 @@
 from kafka import KafkaProducer, KafkaConsumer
 import requests
 import json
+import configparser
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-api_key = '---'
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config.get('API', 'key')
 city_id = '2934246'
 Params = {'appid': api_key , 'id':city_id}
 response = requests.get('http://api.openweathermap.org/data/2.5/weather',params=Params)
